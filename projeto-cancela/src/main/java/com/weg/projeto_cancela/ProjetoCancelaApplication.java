8190e54faef0ec5542bd93ee108eed4c4bcb1288
Nicofranca <nicollas_franca@estudante.sesisenai.org.br>
2026-02-20 19:39:01 -0300

    feat: adiciona projeto ao git

---
+++ projeto-cancela/src/main/java/com/weg/projeto_cancela/ProjetoCancelaApplication.java
@@ -0,0 +1,13 @@
+package com.weg.projeto_cancela;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoCancelaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoCancelaApplication.class, args);
+	}
+
+}
---
+++ projeto-cancela/src/test/java/com/weg/projeto_cancela/ProjetoCancelaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weg.projeto_cancela;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoCancelaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
